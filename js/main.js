4281bb5eb4d5620c9be5fd065baf66020eac7410
Yana-Palam <tana.palam@gmail.com>
2023-07-25 16:27:39 +0300

    added section courses

--- js/main.js
+++ js/main.js
@@ -6,3 +6,24 @@ menuBtn.addEventListener("click", onToggleMenuBtn);
 function onToggleMenuBtn() {
   menuMobile.classList.toggle("menu-open");
 }
+
+// -------------------------
+const stringChecker = function (s, p1, p2) {
+  let p1Idx = 0;
+  let p2Idx = 0;
+
+  for (let i = 0; i < s.length; i += 1) {
+    if (p1Idx < p1.length && s[i] === p1[p1Idx]) {
+      p1Idx += 1;
+    } else if (p2Idx < p2.length && s[i] === p2[p2Idx]) {
+      p2Idx += 1;
+    } else {
+      return false;
+    }
+  }
+
+  return p1Idx === p1.length && p2Idx === p2.length;
+};
+console.log(stringChecker("w27y7", "27", "w7y"));
+console.log(stringChecker("radency", "radency", ""));
+console.log(stringChecker(" Hacker News! ", " Hacker News ", "!"));

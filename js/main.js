b24657935ea2564d4a7bb99e6d92925f6c27cdee
Yana-Palam <tana.palam@gmail.com>
2023-07-28 17:00:51 +0300

    added sections feedback and certificates

--- js/main.js
+++ js/main.js
@@ -7,23 +7,23 @@ function onToggleMenuBtn() {
   menuMobile.classList.toggle("menu-open");
 }
 
-// -------------------------
-const stringChecker = function (s, p1, p2) {
-  let p1Idx = 0;
-  let p2Idx = 0;
+const swiperFeedback = new Swiper(".feedback-slider", {
+  loop: true,
 
-  for (let i = 0; i < s.length; i += 1) {
-    if (p1Idx < p1.length && s[i] === p1[p1Idx]) {
-      p1Idx += 1;
-    } else if (p2Idx < p2.length && s[i] === p2[p2Idx]) {
-      p2Idx += 1;
-    } else {
-      return false;
-    }
-  }
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
 
-  return p1Idx === p1.length && p2Idx === p2.length;
-};
-console.log(stringChecker("w27y7", "27", "w7y"));
-console.log(stringChecker("radency", "radency", ""));
-console.log(stringChecker(" Hacker News! ", " Hacker News ", "!"));
+const swiperCertificates = new Swiper(".certificates-slider", {
+  loop: true,
+  slidesPerView: 3,
+
+  spaceBetween: 20,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});

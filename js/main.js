96c4962eeb9f496363d1c4bf62dc7bfdff5d8518
Yana-Palam <tana.palam@gmail.com>
2023-07-30 18:20:00 +0300

    added section questions

--- js/main.js
+++ js/main.js
@@ -27,3 +27,21 @@ const swiperCertificates = new Swiper(".certificates-slider", {
     clickable: true,
   },
 });
+
+const accordeon = document.querySelector(".accordeon");
+const accordeonTitles = accordeon.querySelectorAll(".accordeon-title");
+
+accordeonTitles.forEach.call(accordeonTitles, function (accordeonTitle) {
+  accordeonTitle.addEventListener("click", function () {
+    const currentText =
+      accordeonTitle.parentElement.querySelector(".accordeon-text");
+    accordeonTitle.classList.toggle("accordeon-title-active");
+    currentText.classList.toggle("accordeon-text-visible");
+
+    if (currentText.classList.contains("accordeon-text-visible")) {
+      currentText.style.maxHeight = currentText.scrollHeight + "px";
+    } else {
+      currentText.style.maxHeight = 0;
+    }
+  });
+});
